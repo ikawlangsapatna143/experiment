bdc3f80f8d0cb4a94b018d60c1c5baa8e12351ec
ikawlangsapatna143 <bansonchristoper@gmaill.com>
2024-06-26 16:28:53 +0800

    Final activity1 CRUD

--- src/main/java/com/pioneers/co/experiment/Controllers/ProductController.java
+++ src/main/java/com/pioneers/co/experiment/Controllers/ProductController.java
@@ -2,12 +2,16 @@ package com.pioneers.co.experiment.Controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pioneers.co.experiment.Model.Product;
+import com.pioneers.co.experiment.NotFoundException.ProductNotFoundException;
 import com.pioneers.co.experiment.Repository.ProductRepository;
 
 @RestController
@@ -26,11 +30,11 @@ public ProductController(ProductRepository repo) {
     return repo.findAll();
    }
 
-
-//     public Product geProduct(Long id){
-//     return repo.findById(id);
-//    }
-
+    @GetMapping("/product/{id}")
+    public Product geProduct(@PathVariable Long id){
+    return repo.findById(id)
+    .orElseThrow(()-> new ProductNotFoundException(id));
+ }
 
 //http:127.0.0.1:8080/product/new
 @PostMapping("/product/new")
@@ -38,4 +42,25 @@ public String addProduct(@RequestBody Product newProduct){
     repo.save(newProduct);
     return "A new product is added. Yey";
 }
+//http:127.0.0.1:8080/product/edit/1
+//Update endpoints
+@PutMapping("/product/edit/{id}")
+public Product updateProduct(@PathVariable Long id, @RequestBody Product newProduct){
+    return repo.findById(id)
+    .map(product ->{
+        product.setProductName(newProduct.getProductName());
+        product.setDescription(newProduct.getDescription());
+        product.setPrice(newProduct.getPrice());
+        return repo.save(product);
+    }).orElseGet(()->{
+        return repo.save(newProduct);
+    });
+}
+    //delete endpoints
+    ////http:127.0.0.1:8080/product/delete/1
+    @DeleteMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable Long id){
+        repo.deleteById(id);
+        return "Product Deleted";
+    }
 }
